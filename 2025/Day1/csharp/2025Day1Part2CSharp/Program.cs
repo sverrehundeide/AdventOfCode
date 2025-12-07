aa7812d495168f767fc94956d9e8d9b063884ff5
Sverre Hundeide <github@hundeide.net>
2025-12-07 19:04:23 +0100

    2025-01 part 2: Initial implementation - csharp linked list

---
+++ 2025/Day1/csharp/2025Day1Part2CSharp/Program.cs
@@ -0,0 +1,112 @@
+﻿var circularDigitsList = GetCircularLinkedListWitDigits1To100();
+circularDigitsList.MoveToDigit(50);
+
+const int digitToCount = 0;
+var numberOfTimesAtZero = File.ReadAllLines("input.txt")
+    .Sum(instructionString => circularDigitsList.Move(instructionString, digitToCount));
+
+Console.WriteLine($"The code is : {numberOfTimesAtZero}");
+return;
+
+static DialDigitsList GetCircularLinkedListWitDigits1To100()
+{
+    var circleWithDigits = new DialDigitsList();
+    foreach (var index in Enumerable.Range(0, 100))
+    {
+        var digit = new Digit(index);
+        if (circleWithDigits.Count > 0)
+        {
+            var lastDigit = circleWithDigits.Last();
+            lastDigit.Next = digit;
+            digit.Previous = lastDigit;
+        }
+        circleWithDigits.AddLast(digit);
+    }
+
+    circleWithDigits.Last().Next = circleWithDigits.First();
+    circleWithDigits.First().Previous = circleWithDigits.Last();
+    return circleWithDigits;
+}
+
+
+internal class Digit(int value)
+{
+    public int Value { get; } = value;
+    public Digit? Next { get; set; }
+    public Digit? Previous { get; set; }
+
+    public static Digit Undefined = new(int.MaxValue);
+}
+
+internal class DialDigitsList : LinkedList<Digit>
+{
+    public Digit Current { get; private set; } = Digit.Undefined;
+
+    public void MoveToDigit(int digit)
+    {
+        var matchingDigit = this.FirstOrDefault(node => node.Value == digit);
+        Current = matchingDigit ?? throw new Exception($"Found no digit node with value {digit}");
+    }
+
+    public int MoveRight()
+    {
+        Current = Current.Next!;
+        return Current.Value;
+    }
+
+    public int MoveLeft()
+    {
+        Current = Current.Previous!;
+        return Current.Value;
+    }
+
+    public int Move(string movementInstructionString, int digitToCount)
+    {
+        var timesAtDigit = 0;
+        var movementInstruction = MovementInstruction.Parse(movementInstructionString);
+        for (var i = 0; i < movementInstruction.Distance; i++)
+        {
+            if (movementInstruction.Direction == Direction.Right)
+            {
+                MoveRight();
+            }
+            else
+            {
+                MoveLeft();
+            }
+            if (Current.Value == digitToCount)
+            {
+                timesAtDigit++;
+            }
+        }
+
+        return timesAtDigit;
+    }
+}
+
+internal class MovementInstruction
+{
+    private static readonly Regex InstructionParser = new("(?<direction>[LR])(?<distance>\\d{1,3})");
+
+    public Direction Direction { get; init; }
+    public int Distance { get; init; }
+
+    private MovementInstruction(Direction direction, int distance)
+    {
+        Direction = direction;
+        Distance = distance;
+    }
+
+    public static MovementInstruction Parse(string stringValue)
+    {
+        var parsed = InstructionParser.Match(stringValue);
+        var parsedDirection = parsed.Groups["direction"].Value == "L" ? Direction.Left : Direction.Right;
+        var parsedDistance = int.Parse(parsed.Groups["distance"].Value);
+        return new MovementInstruction(parsedDirection, parsedDistance);
+    }
+}
+
+internal enum Direction
+{
+    Left, Right
+}
---
+++ 2025/Day1/csharp/2025Day1Part2CSharp/Readme.md
@@ -0,0 +1,9 @@
+# Advent of Code 2025 - Day 1
+https://adventofcode.com/2025/day/1#part2
+
+This piece of code is not the most terse or efficient solution to this problem, but it shows how a bi-directional, circular linked list can be used to model the digits which the dial points at. 
+
+The circular linked list should be easy reason about since it closesly resembles the physical dial described in the problem statement.
+
+It should also be easy to extend the functionality if more requirements are added, like for example showing a visual representation of the dials movement ant position.
+
